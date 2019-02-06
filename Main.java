bbe83ed6ddcd1233460aec3542673622cf0a4b02
Fenniless <fenniless@gmail.com>
2019-02-06 09:46:09 -0500

    sumofinput, and it works

--- Main.java
+++ Main.java
@@ -1,9 +1,38 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Gimmy a number m8!:");
+
+        int n = input.nextInt();
+        int i = 0;
+        int sum = 0;
+
+        for (i = 1; i <= n; i++){
+
+            sum = sum + i;
+
+            
+        }
+        System.out.println(sum);
+
+
+
+
+
+
+
+
+
 
     }
 }
+   
