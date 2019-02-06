00ec4b92965c36f8c7c6b0e4afc5676f4bcfddf2
Thao Nguyen <thaonguyen@zipoders-MacBook-Pro.local>
2019-02-06 11:58:38 -0500

    Finished

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        int sum = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input Number");
+        int n = scan.nextInt();
+        
+        
+        for(int i = 1; i <= n; i++){
+        sum+= i;
+    }
+        System.out.println("The sum is " + sum);
     }
 }
