6770b9d57f84e526912ee5ae6178acf89418589d
Ron Duwell <ronduwell@zipcoders-MacBook-Pro.local>
2019-10-16 17:33:34 -0400

    There is no try

--- SumOfInput.java
+++ SumOfInput.java
@@ -17,10 +17,12 @@ public class SumOfInput
         // initialise instance variables
         sum = 0;
     }
-    
-    public Integer oneToNumber(Integer numberToSum){
 
+    public Integer oneToNumber(Integer numberToSum){
+        int sum = 0;
+        for(int i = numberToSum; i > 0; i--){
+            sum += i;
+        }
         return sum;
     }
-
 }
