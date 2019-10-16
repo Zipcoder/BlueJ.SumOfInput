0d82782f53d4b153997024e89aa46250164e7faa
deltagphys <cty.jgates@gmail.com>
2019-10-16 15:20:53 -0400

    completed oneToNumber() method to pass tests

--- SumOfInput.java
+++ SumOfInput.java
@@ -19,7 +19,11 @@ public class SumOfInput
     }
     
     public Integer oneToNumber(Integer numberToSum){
-
+        Integer sum = 0;
+        
+        for (int i=0; i<=numberToSum; i++) {
+            sum += i;
+        }
         return sum;
     }
 
