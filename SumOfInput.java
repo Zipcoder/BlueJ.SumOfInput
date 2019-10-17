435e1056b8a30105f37cb6f403b2ea088abe320f
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-10-17 13:24:35 -0400

    Finished my lab

--- SumOfInput.java
+++ SumOfInput.java
@@ -17,10 +17,12 @@ public class SumOfInput
         // initialise instance variables
         sum = 0;
     }
-    
-    public Integer oneToNumber(Integer numberToSum){
 
+    public Integer oneToNumber(Integer numberToSum){
+       sum = 0;
+        for(int i=1 ;i<=numberToSum;i++){
+            sum=sum+i;
+        }
         return sum;
     }
-
 }
